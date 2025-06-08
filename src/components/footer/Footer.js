47ab8d692b2e949cefa05ad5c736085553badaaa
sowmyavv2@gmail.com <sowmyavv2@gmail.com>
2025-06-08 14:20:08 -0400

    format

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,8 +5,7 @@ import {Fade} from "react-reveal";
 export default function Footer() {
   return (
     <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-      </div>
+      <div className="footer-div"></div>
     </Fade>
   );
 }
